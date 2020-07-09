d618d2f4a421a82edfaad96e1e70d18e51430bda
조아라(Ara Jo)/서비스개발팀/SKP <araara.jo@sk.com>
2020-07-09 15:29:51 +0900

    page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Nav from './components/nav';
+import Home from './components/home';
+import About from './components/about';
+import Skills from './components/skills';
+import Resume from './components/resume';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Fragment>
+      <Nav />
+      <Home />
+      <About />
+      <Skills />
+      <Resume />
+      <Portfolio />
+      <Contact />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/portfolio.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Portfolio() {
+  return <section>Portfolio</section>;
+}
+
+export default Portfolio;
