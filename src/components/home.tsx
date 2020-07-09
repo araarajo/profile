bf029aeb5f7093b3e94c871db330ddb0f795e4e0
조아라(Ara Jo)/서비스개발팀/SKP <araara.jo@sk.com>
2020-07-09 16:26:45 +0900

    Add Home and Nav

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 function Home() {
-  return <section>Home</section>;
+  return (
+    <header className="masthead">
+      <div className="container h-100">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <div className="col-lg-10 align-self-end">
+            <div className="text-center header-content mx-auto">
+              <h4 className="text-white first-title mb-4">Welcome</h4>
+              <h1 className="header-name text-white text-capitalize mb-0">
+                I'M <span className="element font-weight-bold">A Frontend Developer.</span>
+                <span className="typed-cursor">|</span>
+              </h1>
+              <p className="text-white mx-auto header-desc mt-4">blablabla...</p>
+              <div className="mt-4 pt-2">
+                <a href="#About" className="btn-home btn-outline-custom btn-round-home">
+                  Find Out More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8 align-self-baseline"></div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Home;
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
 
 function Nav() {
-  return <nav>Nav</nav>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          Ara Jo
+        </a>
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto my-2 my-lg-0">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#services">
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#portfolio">
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contact">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
